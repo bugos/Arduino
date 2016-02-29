130d129e52c824f9e6a45b3730c3927a7ed1ad07
George Avgenakis <avgenakisg@gmail.com>
2016-02-29 12:26:03 +0200

    read until text is not empty

--- phpWearableCode/ReadArduinoTextWithPhp.php
+++ phpWearableCode/ReadArduinoTextWithPhp.php
@@ -6,6 +6,22 @@
 $myfile = fopen("text.txt", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("text.txt"));
 fclose($myfile);
+
+
+
+$filename="text.txt";	
+
+
+
+do {
+	$size=filesize($filename);
+    echo $filename . ': ' . $size . ' bytes';
+
+
+} while ($size==15);
+
+
+
 ?>
 
 </body>
